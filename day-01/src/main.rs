76074dec06456ab5cf220d7060849787e7cf278f
Kenneth Collins <dev@kecors.net>
2021-12-01 23:23:16 -0500

    Solve day 1, part 2

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -4,10 +4,12 @@ fn main() {
     let mut input = String::new();
     stdin().read_to_string(&mut input).unwrap();
 
-    let depths : Vec<u32> = input
+    let depths: Vec<u32> = input
         .lines()
         .map(|x| x.trim().parse::<u32>().unwrap())
         .collect();
+
+    // Part 1
 
     let mut last_depth_opt = None;
     let mut increases = 0;
@@ -20,5 +22,27 @@ fn main() {
         last_depth_opt = Some(depth);
     }
 
-    println!("Part 1: There are {} measurements larger than the previous measurement", increases);
+    println!(
+        "Part 1: There are {} measurements larger than the previous measurement",
+        increases
+    );
+
+    // Part 2
+
+    let mut last_sum_opt = None;
+    let mut increases = 0;
+    for j in 2..depths.len() {
+        let sum = depths[j - 2] + depths[j - 1] + depths[j];
+        if let Some(last_sum) = last_sum_opt {
+            if sum > last_sum {
+                increases += 1;
+            }
+        }
+        last_sum_opt = Some(sum);
+    }
+
+    println!(
+        "Part 2: {} sums are larger than the previous sum",
+        increases
+    );
 }
